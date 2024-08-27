115e4e7f5e480e340380306d44cc893e76ff0457
EvgeniyaLut <inspire_evgenia@mail.ru>
2024-08-27 14:48:40 +0500

    Add files via upload

---
+++ Переменные.py
@@ -0,0 +1,8 @@
+from time import thread_time_ns
+
+homework=12
+number_of_hours=1.5
+course_name='Python'
+one_task=number_of_hours/homework
+
+print('Курс:', course_name,", всего задач:", homework, ', затрачено часов:', number_of_hours, ", среднее время выполнения", one_task, 'часа')
